fbdfe0935e28bc358c77cc7dc60b55fc42582604
Corbin McNeill <corbin.mc96@gmail.com>
2015-07-10 21:11:41 -0400

    added libraries

---
+++ .build/uno/Adafruit-GPS-Library/Adafruit_GPS.d
@@ -0,0 +1,14 @@
+.build/uno/Adafruit-GPS-Library/Adafruit_GPS.d .build/uno/Adafruit-GPS-Library/Adafruit_GPS.o: lib/Adafruit-GPS-Library/Adafruit_GPS.cpp \
+ /usr/share/arduino/libraries/SoftwareSerial/SoftwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ lib/Adafruit-GPS-Library/Adafruit_GPS.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
