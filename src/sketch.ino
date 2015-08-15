4e1d21024e510c26bcdc1504f955a272584e3720
Aaron Miller <acmiller273@gmail.com>
2015-08-15 17:52:35 -0400

    Cleaned up sketch.ino

--- src/sketch.ino
+++ src/sketch.ino
@@ -15,44 +15,13 @@ GpsController* gps_controller;
 
 Imu* imu;
 
-extern unsigned int __bss_end;
-extern unsigned int __heap_start;
-extern void* __brkval;
-int freeRam () 
-{
-  int v; 
-  return ((uint16_t)&v) - (((uint16_t)__brkval == 0) ? ((uint16_t)&__bss_end) : ((uint16_t)__brkval)); 
-}
-
 void setup() {
   Serial.begin(9600);
-  // delay(1000);
-  Serial.println(freeRam());
-  // delay(1000);
   receiver = RcReceiver::Create();
-  // Serial.println(freeRam());
-  // delay(1000);
   imu = new Imu();
-  // Serial.println(freeRam());
-  // delay(1000);
   controller = new Controller(imu);
-  Serial.println(freeRam());
-  // delay(1000);
-  Serial.println(23);
-  // Serial.println(freeRam());
-  // delay(1000);
   radio_controller = new RadioController(controller, receiver);
-  // Serial.println(freeRam());
-  // delay(1000);
-  Serial.println(25);
-  // Serial.println(freeRam());
-  // delay(1000);
   gps_controller = GpsController::Create(controller, imu);
-  // Serial.println(freeRam());
-  // delay(1000);
-  Serial.println(27);
-  Serial.println(freeRam());
-  // delay(1000);
 }
 
 void loop() {
